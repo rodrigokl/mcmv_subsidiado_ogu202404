a758777fa2f804402a0825667d54b8c5ba9c97ec
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2024-10-24 17:30:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,21 @@ def format_cnpj(cnpj):
 
 st.set_page_config(page_title='Database MCMV', layout='wide')
 
+
+# Ler o arquivo CSV
+df = pd.read_csv('mcmv_subsidiado_ogu202404.csv', encoding='ISO-8859-1')
+st.title('Análise de Obras por Construtora/Entidade')
+
+# Contar o número de obras para cada construtora/entidade, junto com o CNPJ
+obra_por_construtora = df.groupby(['txt_nome_construtora_entidade', 'txt_cnpj_construtora_entidade']).size().reset_index(name='Quantidade de Obras')
+
+# Exibir obras por construtora sem repetidos
+obras_unicas = obra_por_construtora.drop_duplicates(subset=['txt_nome_construtora_entidade', 'txt_cnpj_construtora_entidade'])
+
+# Exibir o DataFrame em uma tabela no Streamlit
+st.write("Obras por Construtora/Entidade (sem repetição):")
+st.dataframe(obras_unicas)
+
 # Ler o arquivo CSV
 df = pd.read_csv('mcmv_subsidiado_ogu202404.csv', encoding='ISO-8859-1')
 st.title('Análise de Obras por Construtora/Entidade')
@@ -45,6 +60,27 @@ st.write(top_20_por_regiao)
 # Ler o arquivo CSV
 df = pd.read_csv('mcmv_subsidiado_ogu202404.csv', encoding='ISO-8859-1')
 st.title('Análise de Obras por Construtora/Entidade e Estado')
+
+# Contar o número de obras para cada construtora/entidade e região, junto com o CNPJ
+obra_por_uf_construtora = df.groupby(['txt_sigla_uf', 'txt_nome_construtora_entidade', 'txt_cnpj_construtora_entidade']).size().reset_index(name='Quantidade de Obras')
+
+# Aplicar a formatação ao CNPJ
+obra_por_uf_construtora['txt_cnpj_construtora_entidade'] = obra_por_uf_construtora['txt_cnpj_construtora_entidade'].apply(format_cnpj)
+
+# Ordenar as construtoras por região e quantidade de obras (em ordem decrescente)
+obra_por_uf_construtora = obra_por_uf_construtora.sort_values(by=['txt_sigla_uf', 'Quantidade de Obras'], ascending=[True, False])
+
+# Agrupar por região e pegar os 20 primeiros de cada região
+top_20_por_uf = obra_por_uf_construtora.groupby('txt_sigla_uf').head(20).reset_index(drop=True)
+
+# Exibir o resultado no Streamlit
+st.write("Top 20 Construtoras com mais obras por UF:")
+st.write(top_20_por_uf)
+
+st.write('-----')
+# Ler o arquivo CSV
+df = pd.read_csv('mcmv_subsidiado_ogu202404.csv', encoding='ISO-8859-1')
+st.title('Banco geral sem repetidos')
 
 # Contar o número de obras para cada construtora/entidade e região, junto com o CNPJ
 obra_por_uf_construtora = df.groupby(['txt_sigla_uf', 'txt_nome_construtora_entidade', 'txt_cnpj_construtora_entidade']).size().reset_index(name='Quantidade de Obras')
